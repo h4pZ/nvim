9b228963218ed7319f62dd622fc41df9ca1b5abe
h4pZ <h4pz@protonmail.com>
2022-03-01 20:58:21 +0100

    Update nvim

--- lua/user/dap-python.lua
+++ lua/user/dap-python.lua
@@ -1,3 +1,14 @@
--- require('dap-python').setup('/Users/kjvd256/mambaforge/envs/qc-dt2/bin/python')
-require('dap-python').setup('~/.virtualenvs/debugpy/bin/python')
+function os.capture(cmd, raw)
+  local f = assert(io.popen(cmd, 'r'))
+  local s = assert(f:read('*a'))
+  f:close()
+  if raw then return s end
+  s = string.gsub(s, '^%s+', '')
+  s = string.gsub(s, '%s+$', '')
+  s = string.gsub(s, '[\n\r]+', ' ')
+  return s
+end
+
+local python_path = os.capture("which python", false)
+require('dap-python').setup(python_path)
 require('dap-python').test_runner = 'pytest'
