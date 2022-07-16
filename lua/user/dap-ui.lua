291ca43d943c3d16538e7f726b4dfcfc0e41a478
h4pZ <h4pz@protonmail.com>
2022-07-16 21:46:12 +0200

    Update nvim

--- lua/user/dap-ui.lua
+++ lua/user/dap-ui.lua
@@ -8,25 +8,25 @@ require("dapui").setup({
     edit = "e",
     repl = "r",
   },
-  sidebar = {
-    -- You can change the order of elements in the sidebar
-    elements = {
-      -- Provide as ID strings or tables with "id" and "size" keys
-      {
-        id = "scopes",
-        size = 0.35, -- Can be float or integer > 1
-      },
-      { id = "breakpoints", size = 0.25 },
-      { id = "stacks", size = 0.25 },
-      { id = "watches", size = 0.25 },
-    },
-    size = 50,
-    position = "left", -- Can be "left", "right", "top", "bottom"
-  },
-  tray = {
-    elements = { "repl" },
-    size = 15,
-    position = "bottom", -- Can be "left", "right", "top", "bottom"
+  layouts = {                                                                                                                      
+    {                                                                                                                              
+      elements = {                                                                                                                 
+        'scopes',                                                                                                                  
+        'breakpoints',                                                                                                             
+        'stacks',                                                                                                                  
+        'watches',                                                                                                                 
+      },                                                                                                                           
+      size = 40,                                                                                                                   
+      position = 'left',                                                                                                           
+    },                                                                                                                             
+    {                                                                                                                              
+      elements = {                                                                                                                 
+        'repl',                                                                                                                    
+        'console',                                                                                                                 
+      },                                                                                                                           
+      size = 10,                                                                                                                   
+      position = 'bottom',                                                                                                         
+    },                                                                                                                             
   },
   floating = {
     max_height = nil, -- These can be integers or a float between 0 and 1.
