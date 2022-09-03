ba21b278775f94990d680eb4a9439f9ef12cca09
h4pZ <h4pz@protonmail.com>
2022-09-03 21:13:51 +0200

    Update nvim

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -14,6 +14,6 @@ null_ls.setup({
         null_ls.builtins.formatting.black,
         null_ls.builtins.diagnostics.flake8,
         null_ls.builtins.diagnostics.pylint,
-        null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.diagnostics.eslint_d,
     },
 })
