25f6e037b3c4f8fdc41609ae6dfe312c2a6e9a91
h4pZ <h4pz@protonmail.com>
2022-09-03 21:08:21 +0200

    Add mason

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -4,6 +4,9 @@ if not status_ok then
 end
 
 require "user.lsp.lsp-signature"
-require "user.lsp.lsp-installer"
+require "user.lsp.lsp-mason"
 require("user.lsp.handlers").setup()
 require "user.lsp.null-ls"
+-- For some reason it doesn't work.
+vim.keymap.set("n", "gl", vim.diagnostic.open_float)
+
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -84,7 +84,7 @@ return packer.startup(function(use)
 
   -- LSP
   use "neovim/nvim-lspconfig" -- enable LSP
-  use "williamboman/nvim-lsp-installer" -- simple to use language server installer
+  use { "williamboman/mason.nvim" } -- Package Manager.
   use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
   use "simrat39/symbols-outline.nvim"
