ac2c9d2cedf4ae99c77fd49e6ff8642c5e4f3ce7
h4pZ <h4pz@protonmail.com>
2022-10-26 09:36:06 +0200

    Update nvim

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -12,6 +12,9 @@ null_ls.setup({
     sources = {
         null_ls.builtins.formatting.isort,
         null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.xmllint,
+        null_ls.builtins.formatting.fixjson.with({ extra_args = {"--indent", "4"}}),
+        null_ls.builtins.formatting.eslint_d,
         null_ls.builtins.diagnostics.flake8,
         null_ls.builtins.diagnostics.pylint,
         null_ls.builtins.diagnostics.eslint_d,
