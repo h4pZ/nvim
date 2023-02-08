4dab3885e3b81a3280cfd0b11651e6b59471fd12
h4pZ <h4pz@protonmail.com>
2023-02-08 14:14:39 +0100

    Update nvim

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -16,7 +16,7 @@ M.setup = function()
   local config = {
     -- disable virtual text
     virtual_text = {
-            severity = { min = "WARN" ,},
+            severity = { min = "ERROR" ,},
     },
     -- show signs
     signs = {
