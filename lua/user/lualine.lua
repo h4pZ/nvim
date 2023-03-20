705a3476495e58531141e28b26bfbbdc2c98ba24
h4pZ <h4pz@protonmail.com>
2023-03-20 13:55:12 +0100

    Remove tabnine from statusbar

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -112,7 +112,6 @@ lualine.setup({
     },
     sections = {
         lualine_a = { branch, diagnostics },
-        lualine_b = { python_env , "tabnine"},
         lualine_c = { },
         -- lualine_x = { "encoding", "fileformat", "filetype" },
         lualine_x = { diff, spaces, "encoding", filetype },
