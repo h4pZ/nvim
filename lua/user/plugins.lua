fceb824b9a8fda3cc2e7ca2a357b6c3cf2443deb
h4pz <h4pz@protonmail.com>
2023-04-16 13:02:56 +0200

    fix: remove tabnine again ehhe

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -152,9 +152,6 @@ return packer.startup(function(use)
   -- nvim repl
   use {'hkupty/iron.nvim'}
 
-  -- tabnine
-  use { 'codota/tabnine-nvim', run = './dl_binaries.sh' }
-
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
