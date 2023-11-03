0d94b3f609e782a30ef1d741f5d2f937e718d563
h4pz <h4pz@protonmail.com>
2023-11-03 23:24:06 +0100

    Update indentline to version 3

--- lua/user/indentline.lua
+++ lua/user/indentline.lua
@@ -1,4 +1,4 @@
-local status_ok, indent_blankline = pcall(require, "indent_blankline")
+local status_ok, indent_blankline = pcall(require, "ibl")
 if not status_ok then
 	return
 end
@@ -25,7 +25,6 @@ vim.g.indent_blankline_char = "▏"
 vim.g.indent_blankline_show_trailing_blankline_indent = false
 vim.g.indent_blankline_show_first_indent_level = true
 vim.g.indent_blankline_use_treesitter = true
-vim.g.indent_blankline_show_current_context = false
 vim.g.indent_blankline_context_patterns = {
     "class",
     "return",
@@ -63,12 +62,4 @@ vim.cmd [[highlight IndentBlanklineContextStart guisp=#abaec2 gui=underline]]
 -- vim.opt.listchars:append "space:"
 -- vim.opt.listchars:append "eol:↴"
 
-indent_blankline.setup({
-     show_end_of_line = true,
-     space_char_blankline = " ",
-     show_current_context = true,
-     show_current_context_start = false,
-     char_highlight_list = {
-       "IndentBlanklineIndent1",
-     },
-})
+indent_blankline.setup({})
