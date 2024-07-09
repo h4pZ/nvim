7806ff8fad8af537aec27cc060967e71c3ac4ea1
h4pZ <h4pz@protonmail.com>
2024-07-09 16:29:22 +0200

    chore: update diagnostics showing

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,10 +1,37 @@
--- EXAMPLE 
-local on_attach = require("nvchad.configs.lspconfig").on_attach
+-- EXAMPLE
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" , "pyright", "ruff", "clangd", "rust_analyzer", }
+local servers = { "html", "cssls", "pyright", "ruff", "clangd", "rust_analyzer", }
+
+
+local on_attach = function(client, bufnr)
+  -- Call the default on_attach function
+  require("nvchad.configs.lspconfig").on_attach(client, bufnr)
+  local config = {
+    -- disable virtual text
+    virtual_text = false,
+    virtual_lines = false,
+    -- show signs
+    update_in_insert = true,
+    underline = {
+      severity = "ERROR",
+    },
+    severity_sort = true,
+    float = {
+      focusable = false,
+      style = "minimal",
+      border = "rounded",
+      source = "always",
+      header = "",
+      prefix = "",
+    },
+  }
+
+
+  vim.diagnostic.config(config)
+end
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
--- lua/options.lua
+++ lua/options.lua
@@ -6,4 +6,7 @@ local o = vim.o
 o.timeoutlen = 100
 o.relativenumber = true
 
+local opts = { noremap = true, silent = true }
+vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
+
 -- o.cursorlineopt ='both' -- to enable cursorline!
