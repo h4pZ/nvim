c7db8ae4a62cfa97febabb03e7e58ff85936def8
h4pZ <h4pz@protonmail.com>
2024-12-28 22:37:31 -0500

    feat: add snippets trigger

--- lua/configs/cmp.lua
+++ lua/configs/cmp.lua
@@ -24,7 +24,7 @@ end
 local has_words_before = function()
   if vim.api.nvim_get_option_value("buftype", { buf = 0 }) == "prompt" then return false end
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
+  return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
 end
 
 cmp.event:on("menu_opened", function()
@@ -75,7 +75,7 @@ cmp.setup {
   },
   mapping = {
     ["<C-k>"] = cmp.mapping.select_prev_item(),
-		["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<C-j>"] = cmp.mapping.select_next_item(),
     ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
     ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
     ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
@@ -84,6 +84,15 @@ cmp.setup {
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     },
+    ["<C-s>"] = cmp.mapping(function()
+      cmp.complete({
+        config = {
+          sources = {
+            { name = "luasnip" }, -- Only use the LuaSnip source
+          },
+        },
+      })
+    end, { "i", "c" }),
     -- Accept currently selected item. If none selected, `select` first item.
     -- Set `select` to `false` to only confirm explicitly selected items.
     ["<CR>"] = cmp.mapping.confirm { select = true },
