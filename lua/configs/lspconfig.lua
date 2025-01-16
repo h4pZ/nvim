f48e7855e3bd40c09881c5509be8e04045b2589e
h4pZ <h4pz@protonmail.com>
2025-01-16 08:51:07 +0100

    update nvim

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,7 +3,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "pyright", "ruff", "clangd", "rust_analyzer", "vscode-html-language-server", }
+local servers = { "html", "cssls", "pyright", "ruff", "clangd", "rust_analyzer", "vscode-html-language-server", "svelte" }
 
 
 local on_attach = function(client, bufnr)
