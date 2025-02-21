a8085e5d473b26c7fa0450b18f75c0b6bd02dfbb
h4pz <h4pz@protonmail.com>
2025-02-21 23:10:26 +0100

    chore: update nvim

--- lua/options.lua
+++ lua/options.lua
@@ -6,6 +6,12 @@ local o = vim.o
 o.timeoutlen = 100
 o.relativenumber = true
 
+-- Set default indentation to 4 spaces
+o.shiftwidth = 4
+o.tabstop = 4
+o.expandtab = true
+
+
 local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
 
