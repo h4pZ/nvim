323768c09b04d16834be8bb0a85a82d6b92eb7d6
h4pz <h4pz@protonmail.com>
2025-08-21 17:26:24 +0200

    chore: update lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -2,7 +2,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "pyright", "ruff", "clangd", "rust_analyzer", "vscode-html-language-server", "svelte",
+local servers = { "html", "cssls", "pyright", "ruff", "clangd", "rust_analyzer", "svelte",
   "gopls", "golangci_lint_ls" }
 
 
